921175647911af3f0538f30ee361006f282d2cd6
celarno <marcel-arnold@gmx.de>
2018-05-02 13:58:43 +0100

    update

--- connect.php
+++ connect.php
@@ -3,8 +3,7 @@
 session_start();
 $username = $_SESSION['username'];
 
-//$connection = mysqli_connect('localhost', 'root', 'x8C3wEqZd4DdoxwP', 'db_recomm');
-$connection = mysqli_connect('sql312.epizy.com', 'epiz_22019692', 'SN9-SFY-rxS-Ysk', 'epiz_22019692_db_recomm');
+$connection = mysqli_connect('localhost', 'root', 'x8C3wEqZd4DdoxwP', 'db_recomm');
 
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
