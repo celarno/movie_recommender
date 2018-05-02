16243035853a61cb0d41c2498f9d2014008b44b7
celarno <marcel-arnold@gmx.de>
2018-05-02 03:16:14 +0100

    update

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 session_start();
 $username = $_SESSION['username'];
 
-$connection = mysqli_connect('85.10.205.173:3306', 'root_root@locahost', 'x8C3wEqZd4DdoxwP', 'db_recomm');
+$connection = mysqli_connect('localhost', 'root', 'x8C3wEqZd4DdoxwP', 'db_recomm');
 
 if (!$connection){
     die("Database Connection Failed" . mysqli_error($connection));
